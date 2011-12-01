ba6ebe7d8922e116eeca8194cce349542040e972
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2011-12-01 16:39:19 -0600

    Added binaries and install script

---
+++ superpack_installer.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+PYTHON=/usr/bin/python
+SERVER=http://dl.dropbox.com/u/233041/PyMC
+
+echo 'Would you like to install gFortran (recommended, and required if not already installed)? (y/n)'
+read install
+if  [ "$install" == "y" ] || [ "$install" == "Y" ]; then
+  # echo 'Downloading gcc ...'
+  # curl -o gcc-42-5666.3-darwin11.pkg http://r.research.att.com/tools/gcc-42-5666.3-darwin11.pkg
+  # echo 'Installing gcc ...'
+  # sudo installer -pkg 'gcc-42-5666.3-darwin11.pkg' -target '/'  
+  echo 'Downloading gFortran ...'
+  curl -o gfortran-lion-5666-3.pkg http://r.research.att.com/gfortran-lion-5666-3.pkg
+  echo 'Installing gFortran ...'
+  sudo installer -pkg 'gfortran-lion-5666-3.pkg' -target '/'
+
+elif [ "$install" == "n" ] || [ "$install" == "N" ]; then
+  echo 'Skipping gFortran install.'
+else
+  echo 'Did not recognize input. Exiting'
+  exit 0
+fi
+echo 'Downloading ez_setup ...'
+curl -o ez_setup.py http://peak.telecommunity.com/dist/ez_setup.py
+echo 'Installing ez_setup ...'
+sudo "${PYTHON}" ez_setup.py
+echo 'Installing numpy ...'
+sudo "${PYTHON}" -m easy_install -N -Z ${SERVER}/numpy-2.0.0.dev_eb46577_20111130-py2.7-macosx-10.7-x86_64.egg
+echo 'Installing matplotlib ...'
+sudo "${PYTHON}" -m easy_install -N -Z ${SERVER}/matplotlib-1.2.x-py2.7-macosx-10.7-intel.egg
+echo 'Installing scipy ...'
+sudo "${PYTHON}" -m easy_install -N -Z ${SERVER}/scipy-0.11.0.dev_f47d2f1_20111130-py2.7-macosx-10.7-x86_64.egg
+echo 'Installing pandas ...'
+sudo "${PYTHON}" -m easy_install -N -Z ${SERVER}/pandas-0.6.1_20111130-py2.7-macosx-10.7-x86_64.egg
+echo 'Installing pymc ...'
+sudo "${PYTHON}" -m easy_install -N -Z ${SERVER}/pymc-2.2alpha-py2.7-macosx-10.7-intel.egg
+echo 'Installing readline ...'
+sudo "${PYTHON}" -m easy_install -N -Z readline
+echo 'Installing ipython ...'
+sudo "${PYTHON}" -m easy_install -N -Z ${SERVER}/ipython-0.12.beta-py2.7.egg
+echo 'Installing nose ...'
+sudo "${PYTHON}" -m easy_install -N -Z nose
+echo 'Installing DateUtils'
+sudo "${PYTHON}" -m easy_install -N -Z DateUtils
+echo 'Done'
