b3c6999a320e687f064515fe8f395a8b2452a786
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2015-03-25 14:17:31 -0500

    Truncated history

---
+++ README.md
@@ -0,0 +1,26 @@
+# Scipy Superpack for Homebrew
+
+## Recent builds of fundamental Python scientific computing packages for OS X
+
+This shell script will build and install the Python scientific stack, including [Numpy](https://github.com/numpy/numpy), [Scipy](https://github.com/scipy/scipy), [Matplotlib](https://github.com/matplotlib/matplotlib), [IPython](https://github.com/ipython/ipython), [Pandas](https://github.com/pydata/pandas), [Statsmodels](https://github.com/statsmodels/statsmodels), [Scikit-Learn](https://github.com/scikit-learn/scikit-learn), and [PyMC](https://github.com/pymc-devs/pymc) for OS X 10.10 (Yosemite) using the [Homebrew](http://brew.sh) package manager. The script will use recent development code from each package, which means that though some bugs may be fixed and features added, they also may be more unstable than the official releases. 
+
+The SuperPack will install Python 2.7.6 from Homebrew and build all packages against it. 
+
+### Dependencies
+
+OS X 10.10 (Yosemite)
+
+### Install
+
+[Download Scipy Superpack Installer for OSX 10.10](https://raw.github.com/fonnesbeck/ScipySuperpack/master/install_superpack.sh)
+
+To install, open a terminal in the directory that the script is located and call:
+
+> sh install_superpack.sh
+
+If you have not already installed the current Homebrew, Git, and the Xcode command line tools, these will be installed for you.
+
+### Contact
+
+Contact [Chris Fonnesbeck](https://github.com/fonnesbeck) or follow me on Twitter ([@fonnesbeck](http://twitter.com/fonnesbeck))
+
---
+++ install_superpack.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+hash brew &> /dev/null
+if [ $? -eq 1 ]; then
+    echo 'Installing Homebrew ...'
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+# Ensure Homebrew formulae are updated
+brew update
+
+hash git &> /dev/null
+if [ $? -eq 1 ]; then
+    echo 'Installing Git ...'
+    brew install git
+fi
+
+hash gcc &> /dev/null
+if [ $? -eq 1 ]; then
+    echo 'No gcc detected; Installing XCode Command Line Tools ...'
+    xcode-select --install
+fi
+
+# Add science tap
+brew tap homebrew/science
+
+# Python tools and utilities
+brew install python
+brew install gcc
+pip install -U nose
+pip install -U six
+pip install -U patsy
+pip install -U pygments
+pip install -U sphinx
+pip install -U cython
+
+# IPython
+brew install zeromq
+pip install -U jinja2
+pip install -U tornado
+pip install -U pyzmq
+pip install -U git+git://github.com/ipython/ipython.git
+
+# OpenBLAS for NumPy/SciPy
+brew install openblas
+export BLAS=/usr/local/opt/openblas/lib/libopenblas.a
+export LAPACK=/usr/local/opt/openblas/lib/libopenblas.a
+
+# Build from cloned repo to avoid SciPy build issue
+git clone git@github.com:numpy/numpy.git numpy_temp
+cd numpy_temp
+python setupegg.py bdist_egg
+easy_install dist/*egg
+cd ..
+rm -rf numpy_temp
+
+# SciPy
+pip install -U git+git://github.com/scipy/scipy#egg=scipy-dev
+
+# Matplotlib
+brew install freetype
+pip install -U git+git://github.com/matplotlib/matplotlib.git
+
+# Rest of the stack
+pip install -U git+git://github.com/pydata/pandas.git 
+pip install -U git+git://github.com/scikit-learn/scikit-learn.git
+pip install -U git+git://github.com/pymc-devs/pymc.git@2.3
+pip install -U git+git://github.com/statsmodels/statsmodels.git
+pip install -U git+git://github.com/Theano/Theano.git
+
+# Release version of Bokeh
+pip install -U bokeh
